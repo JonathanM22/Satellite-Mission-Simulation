0cbf4a7f7711a1319ed38ed42a9002a0e455d911
JonathanM22 <49132274+JonathanM22@users.noreply.github.com>
2026-01-17 15:05:41 -0500

    Wrote some more functions and quaterions operations

--- GNC.py
+++ GNC.py
@@ -20,16 +20,25 @@ from astropy.coordinates import get_body_barycentric
 class Quaternion:
 
     # q = [vector, scalor]
-    def __init__(self, vector, scalor):
-        self.scalor = scalor
-        self.vector = vector
-        self.q1 = vector[0]
-        self.q2 = vector[1]
-        self.q3 = vector[2]
-        self.q4 = scalor
-        self.value = np.array([self.q1, self.q2, self.q3, self.q4])
+    def __init__(self, q1, q2, q3, q4):
+        self.q1 = q1
+        self.q2 = q2
+        self.q3 = q3
+        self.q4 = q4
+        self.scalor = q4
+        self.vector = np.array([q1, q2, q3])
+        self.value = np.array(
+            [self.q1, self.q2, self.q3, self.q4]).reshape(4, 1)
 
-    def identity(self):
+    def inverse(self):
+        q_mag = np.linalg.norm(self.value)
+        q_conj = np.array([-self.q1, -self.q2, -self.q3, self.q4])
+        q_inv = q_conj / (q_mag**2)
+
+        return Quaternion(q_inv[0], q_inv[1], q_inv[2], q_inv[3])
+
+    @staticmethod
+    def identity():
         return Quaternion(np.zeros(3), 1)
 
     @staticmethod
@@ -60,6 +69,14 @@ class Quaternion:
             [-q1, -q2, -q3]
         ])
 
+    def cross(self, q2):
+        result = np.block([Quaternion.phi(self), self.value]) @ q2.value
+        return Quaternion(result[0], result[1], result[2], result[3])
+
+    def dot(self, q2):
+        result = np.block([Quaternion.eps(self), self.value]) @ q2.value
+        return Quaternion(result[0], result[1], result[2], result[3])
+
 
 def skew_mtx(x_vec):
     x1 = x_vec[0]
@@ -72,6 +89,40 @@ def skew_mtx(x_vec):
                      ])
 
 
+def euler_313(psi, theta, phi):
+    # Appendix B 3-2-1 matrix OR sec 2.9 pg55
+    A11 = np.cos(theta)*np.cos(phi)
+    A12 = np.cos(theta)*np.sin(phi)
+    A13 = -np.sin(theta)
+    A21 = (-np.cos(psi)*np.sin(phi)) + (np.sin(psi)*np.sin(theta)*np.cos(phi))
+    A22 = (np.cos(psi)*np.cos(phi)) + (np.sin(psi)*np.sin(theta)*np.sin(phi))
+    A23 = np.sin(psi)*np.cos(theta)
+    A31 = (np.sin(psi)*np.sin(phi)) + (np.cos(psi)*np.sin(theta)*np.cos(phi))
+    A32 = (-np.sin(psi)*np.cos(phi)) + (np.cos(psi)*np.sin(theta)*np.sin(phi))
+    A33 = np.cos(psi)*np.cos(theta)
+
+    return np.array([A11, A12, A13],
+                    [A21, A22, A23],
+                    [A31, A32, A33])
+
+
+def euler_to_quat(psi, theta, phi):
+    # Appendix B: 3-2-1 -> quaternion
+    q1 = np.cos(phi/2)*np.cos(theta/2)*np.sin(psi/2) - \
+        np.sin(phi/2)*np.sin(theta/2)*np.cos(psi/2)
+
+    q2 = np.cos(phi/2)*np.sin(theta/2)*np.cos(psi/2) + \
+        np.sin(phi/2)*np.cos(theta/2)*np.sin(psi/2)
+
+    q3 = np.sin(phi/2)*np.cos(theta/2)*np.cos(psi/2) - \
+        np.cos(phi/2)*np.sin(theta/2)*np.sin(psi/2)
+
+    q4 = np.cos(phi/2)*np.cos(theta/2)*np.cos(psi/2) + \
+        np.sin(phi/2)*np.sin(theta/2)*np.sin(psi/2)
+
+    return Quaternion(q1, q2, q3, q4)
+
+
 class ReactionWh:
 
     def __init__(self, mass, J_spin, J_perp, wl_unit):
@@ -82,8 +133,12 @@ class ReactionWh:
         self.wl = 0
 
     # Inertia of non-spin axises of reachtion wheel
-    def Jwh_body(self):
+    def Jwh_body_perp(self):
         return self.J_perp*(np.identity(3) - np.outer(self.wl_unit, self.wl_unit))
+
+    # Hwh_body = angular momentum of wheels in body frame
+    def Hwh_body(self, sat_w):
+        return self.J_spin*(np.dot(self.wl_unit, sat_w) + self.wl)*self.wl_unit
 
 
 # Exporting earth_orbit sim data
@@ -128,21 +183,43 @@ wh2 = ReactionWh(1, 3, 6, np.array([0, 1, 0]))
 wh3 = ReactionWh(1, 3, 6, np.array([0, 0, 1]))
 
 # JB = inertia of S/C with Jwh_body
-JB = sat.inertia + wh1.Jwh_body() + wh2.Jwh_body() + wh3.Jwh_body()
+JB = sat.inertia + wh1.Jwh_body_perp() + wh2.Jwh_body_perp() + \
+    wh3.Jwh_body_perp()
 
-# Hwh_B = angular momentum of wheels in body frame
+
+# intial sat position
+psi0 = 60
+theat0 = 0
+phi0 = 0
+q_sat = euler_to_quat(np.deg2rad(psi0), np.deg2rad(theat0), np.deg2rad(phi0))
+sat_w = np.array([3, 0, 0])
 
 
-def Hwh_B(wheels, w_b):
-    for wheel in wheels:
-        total_h += (wheel.J_spin *
-                    (np.dot(wheel.wl_unit, w_b) + wheel.wl))*wheel.wl_unit
+# Commanded sat position
+psi_c = 30
+theat_c = 0
+phi_c = 0
+q_c = euler_to_quat(np.deg2rad(
+    psi_c), np.deg2rad(theat_c), np.deg2rad(phi_c))
 
+q_error = q_sat.cross(q_c.inverse())
+print(q_error.value)
+
+sat_w = np.array([3, 0, 0])
+
+# Total Angular momentum of sat with wheels
+total_wh_h = wh1.Hwh_body(sat_w) + wh2.Hwh_body(sat_w) + wh3.Hwh_body(sat_w)
+sat_h = (JB*sat_w) + total_wh_h
 
 # EOM
-# w_b = angular momentum of body relative to inertial in body frame
-w_b_dot = np.linalg.norm(JB)
+# sat_w = angular momentum of body relative to inertial in body frame
 
+
+def sat_w_dot(JB, Lwh_b, sat_w, total_wh_h):
+    return np.linalg.inv(JB)*(-Lwh_b - (np.cross(sat_w, (JB*sat_w + total_wh_h))))
+
+
+print("done")
 
 # # Create a 3D plot
 # ax = plt.figure().add_subplot(projection='3d')
